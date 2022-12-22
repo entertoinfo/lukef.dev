5967f5f234faa9c0241e031aad793f90cc3382ea
entertoinfo <entertoinfo29@gmail.com>
2022-12-22 12:22:57 +0200

    Bugs fixed 🫡

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const {src, dest, watch, parallel, series} = require('gulp');
-const sass         = require('gulp-sass')(require('sass'));
+
+const scss         = require('gulp-sass')(require('sass'));
 const concat       = require ('gulp-concat');
 const browserSync  = require('browser-sync').create();
 const uglify       = require('gulp-uglify-es').default;
@@ -7,16 +8,16 @@ const autoprefixer = require('gulp-autoprefixer');
 const imagemin     = require('gulp-imagemin');
 const del          = require('del');
 
-function browsersync(){
+function browsersync() {
     browserSync.init({
-        server: {
-            baseDir: "app/"
-        }
+      server : {
+        baseDir: 'app/'
+      }
     });
-}
+  }
 
-function cleanDist(){
-    return del('dist')
+function cleanDist() {
+  return del('dist')
 }
 
 function images(){
@@ -32,47 +33,47 @@ function images(){
             ]
         })
     ]))
-    .pipe(dest('dist/images/'))
+    .pipe(dest('dist/images'))
 }
 
 function scripts(){
-    return src([
-        'node_modules/jquery/dist/jquery.js',
-        'app/js/main.js'
-])
-        .pipe(uglify())
-        .pipe(concat('main.min.js'))
-        .pipe(dest('app/js'))
-        .pipe(browserSync.stream())
+  return src([
+    'node_modules/jquery/dist/jquery.js',
+    'app/js/main.js'
+  ])
+    .pipe(concat('main.min.js'))
+    .pipe(uglify())
+    .pipe(dest('app/js'))
+    .pipe(browserSync.stream())
 }
 
-function styles(){
-    return src('app/scss/style.scss')
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+function styles() {
+    return src('app/scss/**/*')
+        .pipe(scss({outputStyle: 'compressed'}))
         .pipe(concat('style.min.css'))
         .pipe(autoprefixer({
-            overrideBrowserslist: ['last 10 version'],
-            grid: true
+          overrideBrowserslist: ['last 10 version'],
+          grid: true
         }))
         .pipe(dest('app/css'))
         .pipe(browserSync.stream())
-}
+  }
 
-function build (){
-    return src ([
-        'app/css/style.min.css',
-        'app/fonts/**/*',
-        'app/js/main.min.js',
-        'app/*.html'
-    ], {base:'app'})
+function build() {
+  return src([
+    'app/css/style.min.css',
+    'app/fonts/**/*',
+    'app/js/main.min.js',
+    'app/*.html'
+  ], {base: 'app'})
     .pipe(dest('dist'))
 }
 
-function watching(){
-    watch(['app/scss/**/*.scss'], styles) 
-    watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts) 
-    watch(['app/*.html']).on('change', browserSync.reload)
-}
+function watching() {
+  watch(['app/scss/**/*.scss'], styles);
+  watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
+  watch(['app/*.html']).on('change', browserSync.reload);
+  }
 
 exports.browsersync = browsersync;
 exports.cleanDist = cleanDist;
@@ -80,7 +81,6 @@ exports.images = images;
 exports.styles = styles;
 exports.scripts = scripts;
 exports.watching = watching;
-
 
 exports.build = series(cleanDist, images, build);
 exports.default = parallel(styles, scripts, browsersync, watching);
